2ee914943a3d5eaabb149764f277667de3eb4777
Toby <tobyxs91@gmail.com>
2022-03-30 11:24:39 +0200

    1.0

---
+++ script.js
@@ -0,0 +1,20 @@
+let error = document.getElementById("error")
+let passwordone = document.getElementById("password1")
+let passwordtwo = document.getElementById("password2")
+
+let form = document.querySelector(".form")
+
+form.addEventListener("submit", function(e){
+    if (passwordone.value !== passwordtwo.value){
+        console.log(passwordone.value, passwordtwo.value)
+        e.preventDefault()
+        error.textContent = "Note: Passwords Do Not Match";
+} else {
+    let username = document.getElementById("firstname")
+    alert("Thanks " + username.value)
+}
+
+
+}
+
+)
